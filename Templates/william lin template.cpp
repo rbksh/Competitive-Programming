6bcefcf2a8068bab698472c09c69e7402e6b65ff
Shesh Shiromani <rbkshesh@gmail.com>
2026-07-13 23:56:28 +0530

    add william lin cpp for cp template

---
+++ Templates/william lin template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+const int mxN = 2e5;
+int n;
+
+void solve() {
+    // Write your problem logic here
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+}
