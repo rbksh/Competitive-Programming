721b10483aa5c1ac20c76baa646fcaada793392b
Shesh Shiromani <rbkshesh@gmail.com>
2026-07-14 00:04:21 +0530

    add ecenerwala template

---
+++ Templates/ecenerwala template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    // Your code goes here
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
