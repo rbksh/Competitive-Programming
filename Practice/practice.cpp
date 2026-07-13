25b917f65dd179995d840cd2224c4307977ac0a1
Shesh Shiromani <rbkshesh@gmail.com>
2026-07-13 21:39:02 +0530

    c++ lecture series practice + template setup

---
+++ Practice/practice.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+#endif
+    int age=18;
+    cout << "hey, my name is shesh shiromani" << "\n";
+    cout << "my age is " << age << "years old" << endl;
+}
