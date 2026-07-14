353ae61bca31827e4cfd7fcee9d1559261499306
Shesh Shiromani <rbkshesh@gmail.com>
2026-07-14 16:53:19 +0530

    started lecture 3 on conditional statements and loops

---
+++ Practice/AC C++ Playlist/Lecture 3.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+
+#endif
+    //Lecture 3 - Conditional Statements and Loops
+    //ans of a condition is either yes or no
+    //if-else syntax used
+    //to check if a number is positive or negative
+    int n;
+    cin >> n; 
+    if (n>=0) {
+        cout << "positive" << endl;
+    } else {
+        cout << "negative" << endl;
+    }
+    
+}
