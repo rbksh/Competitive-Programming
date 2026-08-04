f3f406a9cd49c2d0b51a9f1d8ac9528f340c8c81
Shesh Shiromani <rbkshesh@gmail.com>
2026-08-04 14:15:53 +0530

    added some important comments for future reference

--- CSES/weird-algorithm-try1*.cpp
+++ CSES/weird-algorithm-try1*.cpp
@@ -10,6 +10,15 @@ int main() {
     //int n;
     long long n;
     cin >> n;
-    cout << n << endl;
+    if (n!=1) {
+        cout << n << " ";
+        if (n%2==0) {
+            n=n/2;
+        } else if (n%2!=0) {
+            n=n*3+1;
+        }
+    }
+    cout << "1" << endl;
 }
 //just for safety, since n can vary from 1 to 10^9, we will use long long int
+//question is based on the mathematical concept of the collatz conjecture, which states that for any positive integer n, the sequence defined by the following rules will always reach 1:
