d5ffd8117fe6d764a206931742a4736407fb551c
Shesh Shiromani <rbkshesh@gmail.com>
2026-08-04 14:00:21 +0530

    2nd try failed

---
+++ CSES/weird-algorithm-try1*.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+#endif
+    //int n;
+    long long n;
+    cin >> n;
+    cout << n << endl;
+}
+//just for safety, since n can vary from 1 to 10^9, we will use long long int
