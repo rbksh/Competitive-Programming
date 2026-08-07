821a0c46979e376a7b8c284b00e6a4b78f98b453
Shesh Shiromani <rbkshesh@gmail.com>
2026-08-08 00:10:26 +0530

    solved 1st problem on cf, accepted

---
+++ Codeforces/watermelon.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+#endif
+    int n;
+    cin >> n;
+    if (n>2 && n%2==0) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+}
