27a4a90b761c3ea56ad2b5ceef50d5d350cd7be5
Shesh Shiromani <rbkshesh@gmail.com>
2026-08-10 02:33:03 +0530

    2nd try at 67th integer problem

---
+++ Codeforces/67th integer problem.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+#endif
+    int t;
+    cin >> t;
+
+    if (t < 1 || t > 6767) {
+        return 0;
+    }
+
+    while (t > 0) {
+        int x;
+        cin >> x;
+
+        if (x >= -67 && x <= 67) {
+            int y = x;
+            cout << y << "\n";
+        }
+
+        t--;
+    }
+
+    return 0;
+}
