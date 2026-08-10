71d2902f9e8b37ee602dd2088e7abc2f97046c83
Shesh Shiromani <rbkshesh@gmail.com>
2026-08-10 14:51:35 +0530

    finished if else module, kept good problem

---
+++ Codechef/type of triangle.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+using namespace std; 
+
+int main() { 
+    int a, b, c; 
+    cin >> a >> b >> c; 
+
+    if (a > 0 && b > 0 && c > 0 && a <= 100 && b <= 100 && c <= 100) { 
+        if (a == b && b == c) { 
+            cout << "Equilateral"; 
+        } 
+        else if (a == b || b == c || a == c) { 
+            cout << "Isosceles"; 
+        } 
+        else { 
+            cout << "Scalene"; 
+        } 
+    } 
+    return 0; 
+}
