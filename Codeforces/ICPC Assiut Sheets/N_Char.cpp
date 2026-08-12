4484092bf50b69965450fdf78677e56c0eef9523
Shesh Shiromani <rbkshesh@gmail.com>
2026-08-12 22:25:33 +0530

    changes

--- Codeforces/ICPC Assiut Sheets/N_Char.cpp
+++ Codeforces/ICPC Assiut Sheets/N_Char.cpp
@@ -5,9 +5,10 @@ int main() {
     char x;
     cin >> x;
     if (int(x) >= 65 && int(x) <= 90) {
-        int(x) = 32 - int(x);
-        cout << "" << char(int(x)) << endl;
-    } else {
+        int(x) = int(x) - 32;
+        cout << char(int(x)) << endl;
+    } else if (int(x)>=97 && int(x)<=122) {
+        int(x) = abs(32-int(x));
         cout << char(int(x)) << endl;
     }
     return 0;
