aaf0b9097b5454d7ed2ea5b300432556f625d71b
Shesh Shiromani <rbkshesh@gmail.com>
2026-08-13 14:51:34 +0530

    solved 3rd prob of contesT

---
+++ Audible_Range.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long t;
+    cin >> t;
+    while (t--) {
+        long long x;
+        cin >> x;
+        if (x>=67 && x<=45000) {
+            cout << "Yes" << endl;
+        } else {
+            cout << "No" << endl;
+        }
+    }
+    return 0;
+}
