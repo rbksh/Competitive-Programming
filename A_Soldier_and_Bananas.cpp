3db459ff49fc210af0076dbd83de4f2e12b1e76c
Shesh Shiromani <rbkshesh@gmail.com>
2026-08-23 23:03:47 +0530

    some cpp compiler stuff

---
+++ A_Soldier_and_Bananas.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <set>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    if(!(cin >> t)) 
+
+    if (t>=1 && t<=25) {
+        while (t--) {
+            int x;
+            cin >> x;
+            int ChefinaAge = x-10;
+            cout << ChefinaAge << endl;
+        }
+    }
+    return 0;
+}
--- Notes/C++ STL - Utkarsh Gupta.cpp
+++ Notes/C++ STL - Utkarsh Gupta.cpp
@@ -1,14 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <algorithm>
-#include <numeric>
-#include <map>
-#include <unordered_map>
-#include <queue>
-#include <set>
-#include <cmath>
-
 
 using namespace std;
 
@@ -16,8 +7,8 @@ int main() {
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
+    #endif
 
-#endif
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
@@ -26,14 +17,10 @@ int main() {
     cout << v[0] << endl;
 
     long long t;
-    if(!(cin >> t)) return 0;
+    if (!(cin >> t)) return 0;
     while (t--) {
-        //solution goes here
+        // Solution code goes here
     }
 
-
-    //vector - dynamic sized array(meaning the length of that array can be altered or the elements inside it can be changed or removed.)
-    vector <int> v(10,'h'); //empty vector of integers
-    cout << v[0] << endl;
-
+    return 0;
 }
