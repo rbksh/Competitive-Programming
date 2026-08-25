35357ee04c819edf30e69905dbb4374f7b2ef518
Shesh Shiromani <rbkshesh@gmail.com>
2026-08-25 23:18:23 +0530

    first try at this lowkey tuff prob on cf

---
+++ A_Tram.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <set>
+#include <cmath>
+
+using namespace std;
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    if(!(cin >> n)) return 0;
+    while (n--) {
+        int a,b;
+        cin >> a >> b;
+        
+    }
+
+
+
+
+}
