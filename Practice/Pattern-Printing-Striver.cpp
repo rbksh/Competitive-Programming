2e6ededd9c514a0e52165b31ce92c2b6ead7c1ff
Shesh Shiromani <rbkshesh@gmail.com>
2026-08-27 01:38:11 +0530

    finished half of pattern printing video from striver, moving on to maths for dsa, will complete pattern printing tomorrow, long weekend lmao

--- Practice/Pattern-Printing-Striver.cpp
+++ Practice/Pattern-Printing-Striver.cpp
@@ -54,17 +54,17 @@ void printPattern4(int z) {
 //Pattern 5 - Inverted star triangle
 void printPattern5(int a) {
     for (int i =1; i<=a;i++) {
-        for (int j =1 ; j<=n-i+1;j++) {
+        for (int j =1 ; j<=a-i+1;j++) {
             cout << "*";
         }
     }
     cout << endl;
 }
 
-//Pattern 7 - 12345,1234,123,12 and so on 
-void printPattern5(int b) {
+//Pattern 6 - 12345,1234,123,12 and so on 
+void printPattern6(int b) {
     for (int i = 1; i<=b;i++) {
-        for (int j = 1; j<=n-i+1;j++) {
+        for (int j = 1; j<=b-i+1;j++) {
             cout << j << endl;
         }
     }
@@ -74,8 +74,41 @@ void printPattern5(int b) {
 //another possibility of pattern 5 - 55555,4444,333,22,1 
 void printExtraPattern(int c) {
     for (int i = 1; i<=c; i++) {
-        for (int j = 1; j<=n-i+1;j++) {
+        for (int j = 1; j<=c-i+1;j++) {
             cout << i << endl;
+        }
+    }
+    cout << endl;
+}
+
+//Pattern 7 - pascal's triangle using stars lmao
+void printPattern7(int d) {
+    for (int i = 0; i<=d;i++) {
+        for (int j = 0; j<=n-i-i;j++) {
+            cout << " ";
+        }
+        for (int j = 0; j<=2*i+1; j++) {
+            cout << "*";
+        }
+        for (int j = 0; j<=n-i-1;j++) {
+            cout << " ";
+        }
+    }
+    cout << endl;
+
+}
+
+//Pattern 8 - inverted pascal's triangle using stars lmao 
+void printPattern8(int e) {
+    for (int i = 0; i<=e, i++) {
+        for (int j = 0; j<i; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j<=2n-(2i+1);j++) {
+            cout << "*";
+        }
+        for (int j = 0; j<i,j++) {
+            cout << " ";
         }
     }
     cout << endl;
@@ -102,6 +135,22 @@ int main() {
     int a;
     cin >> a;
     printPattern5(a);
+    
+    int b;
+    cin >> b;
+    printPattern6(b);
+    
+    int c;
+    cin >> c;
+    printExtraPattern(c);
+    
+    int d;
+    cin >> d;
+    printPattern7(d);
+    
+    int e;
+    cin >> e;
+    printPattern8(e);
     
 }
 
