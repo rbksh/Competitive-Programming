8a3e621c40d8805e953e6249ed2f2aef3552d703
Shesh Shiromani <rbkshesh@gmail.com>
2026-09-06 14:03:40 +0530

    added some more solved probs to the specific icp mock contest foldeR

--- Codeforces/ICP-Mock-Contest/R_Reorder_Digits.cpp
+++ Codeforces/ICP-Mock-Contest/R_Reorder_Digits.cpp
@@ -1,14 +1,7 @@
+#include <iostream>
 #include <vector>
 #include <string>
 #include <algorithm>
-#include <numeric>
-#include <map>
-#include <unordered_map>
-#include <queue>
-#include <set>
-#include <cmath>
-#include <iostream>
-#include <iomanip>
 
 using namespace std;
 
@@ -16,13 +9,28 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    long long n;
-    cin >> n;
+    string s;
+    if (!(cin >> s)) return 0;
 
+    vector<char> even_indices;
+    vector<char> odd_indices;
+
+    for (int i = 0; i < (int)s.length(); i++) {
+        if (i % 2 == 0) {
+            even_indices.push_back(s[i]);
+        } else {
+            odd_indices.push_back(s[i]);
+        }
+    }
     
 
+    reverse(odd_indices.begin(), odd_indices.end());
 
+    even_indices.insert(even_indices.end(), odd_indices.begin(), odd_indices.end());
+
+    for (char x : even_indices) {
+        cout << x;
+    }
 
     return 0;
-
 }
